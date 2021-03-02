3df3bb09f5eeddc96813550e61fcdd004b97f29c
mukundajmera94 <mukundajmera94@gmail.com>
2021-03-02 19:20:44 +0530

    adding Tree problems

---
+++ Tree/Check for Balanced Tree.py
@@ -0,0 +1,114 @@
+# User function Template for python3
+
+
+'''class Node:
+    # Constructor to create a new Node
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None'''
+
+
+def height(root):
+    # base condition when binary tree is empty
+    if root is None:
+        return 0
+    return max(height(root.left), height(root.right)) + 1
+
+
+def isBalanced(root):
+    # Base condition
+    if root is None:
+        return True
+
+    # for left and right subtree height
+    lh = height(root.left)
+    rh = height(root.right)
+
+    # allowed values for (lh - rh) are 1, -1, 0
+    if (abs(lh - rh) <= 1) and isBalanced(
+            root.left) is True and isBalanced(root.right) is True:
+        return True
+
+
+# {
+#  Driver Code Starts
+# Initial Template for Python 3
+
+from collections import deque
+
+
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        if isBalanced(root):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
